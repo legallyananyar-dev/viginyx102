8bff2d08fe7bac2c2419009bcecd79a445ffe259
vaibhavugale <ugalevaibhav00@gmail.com>
2026-06-13 20:16:31 +0530

    refactor: replace hardcoded Firebase configuration with environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjMCDAeujRLdQJwnCgZrl9hePeYJZ-3UM",
-  authDomain: "viginyx-21488.firebaseapp.com",
-  projectId: "viginyx-21488",
-  storageBucket: "viginyx-21488.firebasestorage.app",
-  messagingSenderId: "172611303385",
-  appId: "1:172611303385:web:402b8bfe968321fc8f8bc8",
-  measurementId: "G-XVXE9WRV7W",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
