82aedf46d603eaa49dedc56e6994e489e3e189a5
legallyananyar-dev <legally.ananyar@gmail.com>
2026-06-13 13:47:32 +0000

    modified:   src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjMCDAeujRLdQJwnCgZrl9hePeJYJ-3UM",
+  apiKey: "AIzaSyAjMCDAeujRLdQJwnCgZrl9hePeYJZ-3UM",
   authDomain: "viginyx-21488.firebaseapp.com",
   projectId: "viginyx-21488",
   storageBucket: "viginyx-21488.firebasestorage.app",
   messagingSenderId: "172611303385",
   appId: "1:172611303385:web:402b8bfe968321fc8f8bc8",
+  measurementId: "G-XVXE9WRV7W"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
